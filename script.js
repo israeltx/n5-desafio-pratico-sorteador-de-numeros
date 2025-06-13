a9feb2079fd4a8c7f484cc2e3ac1c901aff72d17
Israel Teixeira <israeltx@gmail.com>
2025-06-13 12:27:17 -0300

    Style de "sortear" button hover state; Add title to the html

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-const form = document.querySelector('form')
+// Toggle Button
 const toogle_button = document.getElementById('toogle-button')
 const toogle_span = document.getElementById('toogle-span')
 
+// Draw Form
+const form = document.querySelector('form')
+const sortear_button = document.getElementById('sortear-button')
+const button_arrow = document.getElementById('button-arrow')
+
+// Set toogle button's function
 toogle_button.addEventListener('click', (event) => {
   event.preventDefault()
   toogle_button.classList.toggle('toogle-button-is-on')
   toogle_span.classList.toggle('toogle-button-is-on')
 })
 
+// Add animtaion to the button's arrow icon
+sortear_button.addEventListener('mouseover', () => {
+  button_arrow.classList.add('arrow-bounce')
+})
+
+// Remove animtaion to the button's arrow icon
+sortear_button.addEventListener('mouseout', () => {
+  button_arrow.classList.remove('arrow-bouce')
+})
+
+// Sortear button's functionality
+sortear_button.addEventListener('click', (event) => {
+  event.preventDefault()
+})
