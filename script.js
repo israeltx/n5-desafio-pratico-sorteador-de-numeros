087cecf77fd9489b4f3edf554b6259b9e7abf680
Israel Teixeira <israeltx@gmail.com>
2025-06-13 15:44:21 -0300

    Add comments to the script

--- script.js
+++ script.js
@@ -3,26 +3,26 @@ const toogle_button = document.getElementById('toogle-button')
 const toogle_span = document.getElementById('toogle-span')
 const h1 = document.querySelector('h1')
 
-window.addEventListener('load', (event) => {
-  if (window.innerWidth > 390) {
-    h1.classList.remove('display-medium')
-    h1.classList.add('display-large')
-  }
-  
-})
-
 // Draw Form
 const form = document.querySelector('form')
 const sortear_button = document.getElementById('sortear-button')
 const button_arrow = document.getElementById('button-arrow')
 
-// Set toogle button's function
-toogle_button.addEventListener('click', (event) => {
-  event.preventDefault()
-  toogle_button.classList.toggle('toogle-button-is-on')
-  toogle_span.classList.toggle('toogle-button-is-on')
+
+
+// LAYOUT
+// Swap h1 classes
+window.addEventListener('load', (event) => {
+  if (window.innerWidth > 390) {
+    h1.classList.remove('display-medium')
+    h1.classList.add('display-large')
+  }
+
 })
 
+
+
+// ANIMATION
 // Add animtaion to the button's arrow icon
 sortear_button.addEventListener('mouseover', () => {
   button_arrow.classList.add('arrow-bounce')
@@ -32,6 +32,17 @@ sortear_button.addEventListener('mouseover', () => {
 sortear_button.addEventListener('mouseout', () => {
   button_arrow.classList.remove('arrow-bouce')
 })
+
+
+
+// FUNCTIONALITY
+// Set toogle button's function
+toogle_button.addEventListener('click', (event) => {
+  event.preventDefault()
+  toogle_button.classList.toggle('toogle-button-is-on')
+  toogle_span.classList.toggle('toogle-button-is-on')
+})
+
 
 // Sortear button's functionality
 sortear_button.addEventListener('click', (event) => {
