543ea93b261aa25ca1678c86f5fac5d9417db848
Israel Teixeira <israeltx@gmail.com>
2025-06-13 11:39:16 -0300

    Add animation on toggle button click; Correct hover state in the span

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('teste');
+const form = document.querySelector('form')
+const toogle_button = document.getElementById('toogle-button')
+const toogle_span = document.getElementById('toogle-span')
+
+toogle_button.addEventListener('click', (event) => {
+  event.preventDefault()
+  toogle_button.classList.toggle('toogle-button-is-on')
+  toogle_span.classList.toggle('toogle-button-is-on')
+})
+
